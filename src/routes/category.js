76e0b6582237938ea988092c16d95b578125fc73
Jose Lecio <jlercio@hotmail.com>
2024-02-09 22:22:21 -0300

    Create category.js

---
+++ src/routes/category.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const CategoryController = require("../controllers/CategoryController.js");
+
+router.get("/", CategoryController.findAll);
+router.get("/:id", CategoryController.find);
+router.post("/", CategoryController.create);
+router.put("/:id", CategoryController.update);
+router.delete("/:id", CategoryController.delete);
+
+module.exports = router;
